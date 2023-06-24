995ee8607f74dcf92de1c609e71d0696c41e6c4e
DimonDB <135203169+DimonDB@users.noreply.github.com>
2023-06-24 10:23:38 +0300

    Update data_store.py

--- data_store.py
+++ data_store.py
@@ -17,24 +17,21 @@ class Tools(Base):
     profile_id = sq.Column(sq.Integer, primary_key=True)
     unique_id = sq.Column(sq.Integer, unique=True)
 
-    def __init__ (self, engine):
-        self.engine = engine
-
 # добавление записи в БД
-    def add_bd_user (self, profile_id, unique_id):
-        with Session(engine) as session:
-            to_bd = Tools(profile_id=profile_id, unique_id=unique_id)
-            session.add(to_bd)
-            session.commit()
+def add_bd_user (self, profile_id, unique_id):
+    with Session(engine) as session:
+        to_bd = Tools(profile_id=profile_id, unique_id=unique_id)
+        session.add(to_bd)
+        session.commit()
 
 # извлечение записей из БД
-    def user_check(self, profile_id, unique_id):
-        with Session(engine) as session:
-            return (session.query(Tools).filter(Tools.profile_id == profile_id, Tools.unique_id == unique_id).first()
+def user_check(self, profile_id, unique_id):
+    with Session(engine) as session:
+        return (session.query(Tools).filter(Tools.profile_id == profile_id, Tools.unique_id == unique_id).first()
             )
 # удаление таблицы
-    def drop_bd_user(self):
-        Base.metadata.drop_all(self)
+def drop_bd_user(self):
+    Base.metadata.drop_all(self)
 
 if __name__ == '_main__':
     engine = create_engine(db_url_object)
